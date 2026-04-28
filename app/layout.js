f6e9fc3326547a2befa168440a35642464038087
suharyantojp <suharyanto1990@gmail.com>
2026-04-28 15:05:48 +0700

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Jadwal Shalat",
+  description: "Aplikasi jadwal shalat harian"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  );
+}
